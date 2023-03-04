dd216c80ea3a3b76a0fd494217cd3079db6ff0e1
student <student@admins-MacBook-Air-2.local>
2023-03-04 15:04:40 -0500

    Database practice

--- db-practice/app.py
+++ db-practice/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -6,22 +6,45 @@ app = Flask(__name__)
 @app.route('/')
 
 def index():
-    return render_template("index.html")
+    engineers = get_all_engineers()
+    
+    return render_template('index.html', engineers = engineers)
 
 @app.route('/success', methods = ["POST"])
 def submit():
     name = request.form['name']
-    age = request.form['name']
+    age = int(request.form['age'])
     height = request.form['height']
     group = request.form['group']
 
-    conn = squlite3.connect('./static/data/team-edge.db')
+    add_engineer(name, age, height, group)
+
+    return render_template('success.html')    
+
+def add_engineer(name, age, height, group):
+    conn = sqlite3.connect('./static/data/team-edge.db')
     curs = conn.cursor()
-    curs.execute("INSERT INTO engineers(name, age, height, group_num VALUES (7, 7, 7, 7)", (name, age, height, group))
+    curs.execute("INSERT INTO engineers(name, age, height, group_num) VALUES (?, ?, ?, ?)", (name, age, height, group))
     conn.commit()
     conn.close()
 
-    return render_template('success.html')
+def get_all_engineers():
+    conn = sqlite3.connect('./static/data/team-edge.db')
+    curs = conn.cursor()
+    result = curs.execute("SELECT * FROM engineers")
+    engineers = []
+    for row in result:
+        engineer = {
+            'name': row[0],
+            'age': row[1],
+            'height': row[2],
+            'group': row[3]
+        }
+        print(engineer)
+        engineers.append(engineer)
+    conn.close()
+    return engineers
+
 
 if __name__== '__main__':
     app.run(debug=True, host ='0.0.0.0', port = 8000)
